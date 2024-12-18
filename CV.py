666fe47f6608b5e763d8713f66addee1679ca2ff
joe <a7g2amer@gmail.com>
2024-12-18 04:49:11 +0200

    inital commit

--- CV.py
+++ CV.py
@@ -1,11 +1,13 @@
 from fpdf import FPDF
+from fpdf.enums import XPos, YPos
 
 class PDF(FPDF):
     def header(self):
-        self.set_font("Helvetica", "B", 18)
-        self.set_text_color(0, 0, 0)
-        self.cell(0, 10, "Youssif Khalid - CV", border=0, ln=1, align="C")
-        self.ln(4)
+        if self.page_no() == 1:
+            self.set_font("Helvetica", "B", 18)
+            self.set_text_color(0, 0, 0)
+            self.cell(0, 10, "Youssif Khalid - CV", border=0, new_x=XPos.LMARGIN, new_y=YPos.NEXT, align="C")
+            self.ln(4)
 
     def footer(self):
         self.set_y(-15)
@@ -13,37 +15,36 @@ class PDF(FPDF):
         self.set_text_color(0, 0, 0)
         self.cell(0, 10, f"Page {self.page_no()}", align="C")
 
-
 def add_section_title(pdf, title):
     pdf.set_font("Helvetica", "B", 14)
     pdf.set_text_color(0, 51, 102)
-    pdf.cell(0, 10, title, ln=True)
+    pdf.cell(0, 10, title, new_x=XPos.LMARGIN, new_y=YPos.NEXT)
     pdf.set_text_color(0, 0, 0)
-    pdf.ln(2)
-
+    pdf.ln(4)
 
 def add_project_with_icon(pdf, title, description, link, icon_path):
     pdf.set_font("Helvetica", "B", 12)
     pdf.set_text_color(0, 51, 102)
-    
+
     x = pdf.get_x()
     y = pdf.get_y()
-    icon_width = 5  
-    icon_height = 5 
-    pdf.image(icon_path, x=x, y=y + 1, w=icon_width, h=icon_height, link=link)
-    
+    icon_width = 5
+    icon_height = 5
 
-    pdf.set_x(x + icon_width + 2)
-    pdf.cell(0, 10, title, ln=True, link=link)
-    
 
+    try:
+        pdf.image(icon_path, x=x, y=y + 1, w=icon_width, h=icon_height, link=link)
+        pdf.set_x(x + icon_width + 2)
+    except FileNotFoundError:
+        pdf.set_x(x)
+
+    pdf.cell(0, 10, title, new_x=XPos.LMARGIN, new_y=YPos.NEXT, link=link)
     pdf.set_text_color(0, 0, 0)
     pdf.set_font("Helvetica", "", 11)
     pdf.multi_cell(0, 8, description)
     pdf.ln(5)
 
-
-skills = "Python, C++, Java, Dart, Bash, Unix/Linux, GIT/GitHub, Docker/Kubernetes, SQL/MySQL"
+skills = "Python, C++, Java, Dart, Bash, Unix/Linux, GIT/GitHub, Docker/Kubernetes, SQL/MySQL\n"
 languages = "- Arabic: First Language\n- English: C1 Advanced"
 
 projects = [
@@ -72,7 +73,7 @@ projects = [
      "https://github.com/yousef2342K/MY_CV_USING_PFDF")
 ]
 
-# Create PDF
+
 pdf = PDF()
 pdf.add_page()
 pdf.set_font("Helvetica", size=12)
@@ -80,10 +81,10 @@ pdf.set_font("Helvetica", size=12)
 # Contact Information
 pdf.set_font("Helvetica", "I", 10)
 pdf.set_text_color(0, 0, 0)
-pdf.cell(0, 10, "Smouha, Alexandria 21918 - 01064004599 - a7g2amer@gmail.com", ln=True, align="C")
-pdf.cell(0, 10, "LinkedIn: https://www.linkedin.com/in/youssif-khalid-654b872a5", ln=True, align="C", link="https://www.linkedin.com/in/youssif-khalid-654b872a5")
-pdf.cell(0, 10, "GitHub: https://github.com/yousef2342K", ln=True, align="C", link="https://github.com/yousef2342K")
-pdf.ln(5)
+pdf.cell(0, 10, "Smouha, Alexandria 21918 - 01064004599 - a7g2amer@gmail.com", new_x=XPos.LMARGIN, new_y=YPos.NEXT, align="C")
+pdf.cell(0, 10, "LinkedIn: https://www.linkedin.com/in/youssif-khalid-654b872a5", new_x=XPos.LMARGIN, new_y=YPos.NEXT, align="C", link="https://www.linkedin.com/in/youssif-khalid-654b872a5")
+pdf.cell(0, 10, "GitHub: https://github.com/yousef2342K", new_x=XPos.LMARGIN, new_y=YPos.NEXT, align="C", link="https://github.com/yousef2342K")
+pdf.ln(8)
 
 # Experience Section
 add_section_title(pdf, "Experience")
@@ -91,12 +92,14 @@ pdf.set_font("Helvetica", size=10)
 pdf.multi_cell(0, 8, "Semicolon\nJanuary 2023 to April 2023 - Machine Learning Intern\n- Developed machine learning models and algorithms for predictive analytics.\n- Conducted data pre-processing operations such as feature engineering, normalization, and imputation.")
 pdf.ln(2)
 pdf.multi_cell(0, 8, "I Code\nJune 2022 to October 2022 - Flutter Intern\n- Proficient in Flutter framework and Dart programming language.\n- Strong understanding of mobile app development concepts and best practices.\n- Experience with RESTful APIs, Firebase, and other relevant technologies.")
-pdf.ln(2)
+pdf.ln(4)
 
 # Projects Section
 add_section_title(pdf, "Projects")
-icon_path = "github.png"  
-for title, description, link in projects:
+icon_path = "github.png"
+for i, (title, description, link) in enumerate(projects):
+    if i == 2:  
+        pdf.add_page()
     add_project_with_icon(pdf, title, description, link, icon_path)
 
 # Skills Section
@@ -105,7 +108,8 @@ pdf.set_font("Helvetica", size=10)
 pdf.multi_cell(0, 8, skills)
 
 # Languages Section
-add_section_title(pdf, "Languages")
+add_section_title(pdf, "languages")
+pdf.set_font("Helvetica", size=10)
 pdf.multi_cell(0, 8, languages)
 
 
